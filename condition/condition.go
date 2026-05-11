a41e8a1b5fb4597ed5990750b5e3f4c389b0f3fd
rakibwebdev23 <rh.rakibhasan365@gmail.com>
2026-05-11 17:32:34 +0600

    condition update

--- condition/condition.go
+++ condition/condition.go
@@ -9,4 +9,31 @@ func main() {
 	} else {
 		fmt.Println("Adult")
 	}
+
+	// if-else if-else
+	score := 85
+	if score >= 90 {
+		fmt.Println("Grade: A")
+	} else if score >= 80 {
+		fmt.Println("Grade: B")
+	} else if score >= 70 {
+		fmt.Println("Grade: C")
+	} else if score >= 60 {
+		fmt.Println("Grade: D")
+	} else {
+		fmt.Println("Grade: F")
+	}
+
+	// map condiont 
+	useAccess := map[string]bool{
+		"admin": true,
+		"user":  false,
+	}
+
+	role := "admin"
+	if hasAccess, exists := useAccess[role]; exists && hasAccess {
+		fmt.Printf("%s has access\n", role)
+	} else {
+		fmt.Printf("%s does not have access\n", role)
+	}
 }
