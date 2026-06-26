b219ddf3fb65cd4ed76134aeb425194134cab6ac
rakibwebdev26 <rakibhasan.bdcalling@gmail.com>
2026-06-26 17:17:06 +0600

    clouser funtion learn

---
+++ go-clouser/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+const a = 10;
+var p = 100;
+
+func outer () func() {
+	money := 100;
+	age := 30;
+
+	fmt.Println("total age", age)
+
+	show := func ()  {
+		money = money + a + p;
+		println("show funciton total money with age", money);
+	}
+
+	return show
+} 
+
+func call (){
+	incr1 := outer();
+	incr1();
+	incr1();
+	incr1();
+	incr1();
+
+	incr2 := outer();
+	incr2();
+	incr2()
+}
+
+func main (){
+	call();
+}
